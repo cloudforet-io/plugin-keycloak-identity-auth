0c55a3475f06307106749ed6c9078b0c26adee3d
Jongmin Kim <whdalsrnt@megazone.com>
2022-02-08 22:42:34 +0900

    build: remove build scripts

--- bin/build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-# Build a docker image
-DEFAUKT=0.1
-VER=${1:-$DEFAULT}
-cd ..
-docker build -t pyengine/plugin-keycloak-identity-auth .
-docker tag pyengine/plugin-keycloak-identity-auth pyengine/plugin-keycloak-identity-auth:${VER}
--- bin/push.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# How to upload
-DEFAULT=0.1
-VER=${1:-$DEFAULT}
-./build.sh ${VER}
-docker push pyengine/plugin-keycloak-identity-auth:${VER}
